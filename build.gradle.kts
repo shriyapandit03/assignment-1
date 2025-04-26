fcae6da1d7f87ceb8329cb6faac7e49081a7ff3d
shriyapandit03 <shriyapandit03@gmail.com>
2025-04-26 17:38:16 +0530

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath(kotlin("gradle-plugin", version = "1.9.22"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+*****
